068d3334d185141198152b26f011afa86984b10d
Danylo <bodnar.dp@gmail.com>
2022-05-18 22:52:31 +0300

    botstrap added

--- src/components/chess-table/chess-table.js
+++ src/components/chess-table/chess-table.js
@@ -4,126 +4,126 @@ function ChessTable(){
 
     return(
         <div className="container">
-            <table class="chess-board">
+            <table className="chess-board">
             <tbody>
                 <tr>
-                    <td class="light">
-                        <div class="numeric-font numeric-pos dark-font">8</div>
+                    <td className="light">
+                        <div className="numeric-font numeric-pos dark-font">8</div>
                     </td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
                 </tr>
                 <tr>
-                    <td class="dark">
-                        <div class="numeric-font numeric-pos light-font">7</div>
+                    <td className="dark">
+                        <div className="numeric-font numeric-pos light-font">7</div>
                     </td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
                 </tr>
                 <tr>
-                    <td class="light">
-                        <div class="numeric-font numeric-pos dark-font">6</div>
+                    <td className="light">
+                        <div className="numeric-font numeric-pos dark-font">6</div>
                     </td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"><img src="https://images.chesscomfiles.com/chess-themes/pieces/gothic/150/bp.png"
-                            class=".chess-piece" id="pawn" alt="black pawn"/></td>
-                    <td class="dark"><img src="https://images.chesscomfiles.com/chess-themes/pieces/gothic/150/bq.png"
-                        class=".chess-piece" id="queen" alt="black queen"/></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"><img src="https://images.chesscomfiles.com/chess-themes/pieces/gothic/150/bp.png"
+                            className=".chess-piece" id="pawn" alt="black pawn"/></td>
+                    <td className="dark"><img src="https://images.chesscomfiles.com/chess-themes/pieces/gothic/150/bq.png"
+                        className=".chess-piece" id="queen" alt="black queen"/></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
                 </tr>
                 <tr>
-                    <td class="dark">
-                        <div class="numeric-font numeric-pos light-font">5</div>
+                    <td className="dark">
+                        <div className="numeric-font numeric-pos light-font">5</div>
                     </td>
-                    <td class="light"></td>
-                    <td class="dark"><img src="https://images.chesscomfiles.com/chess-themes/pieces/gothic/150/wr.png"
-                        class=".chess-piece" id="rook" alt="qwhite rook"/></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"><img src="https://images.chesscomfiles.com/chess-themes/pieces/gothic/150/bp.png"
-                            class=".chess-piece" id="pawn" alt="ableck pawn"/></td>
+                    <td className="light"></td>
+                    <td className="dark"><img src="https://images.chesscomfiles.com/chess-themes/pieces/gothic/150/wr.png"
+                        className=".chess-piece" id="rook" alt="qwhite rook"/></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"><img src="https://images.chesscomfiles.com/chess-themes/pieces/gothic/150/bp.png"
+                            className=".chess-piece" id="pawn" alt="ableck pawn"/></td>
                 </tr>
                 <tr>
-                    <td class="light">
-                        <div class="numeric-font numeric-pos dark-font">4</div>
+                    <td className="light">
+                        <div className="numeric-font numeric-pos dark-font">4</div>
                     </td>
-                    <td class="dark"><img src="https://images.chesscomfiles.com/chess-themes/pieces/gothic/150/br.png"
-                        class=".chess-piece" id="rook" alt="black rook"/></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
+                    <td className="dark"><img src="https://images.chesscomfiles.com/chess-themes/pieces/gothic/150/br.png"
+                        className=".chess-piece" id="rook" alt="black rook"/></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
                 </tr>
                 <tr>
-                    <td class="dark">
-                        <div class="numeric-font numeric-pos light-font">3</div>
+                    <td className="dark">
+                        <div className="numeric-font numeric-pos light-font">3</div>
                     </td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"><img src="https://images.chesscomfiles.com/chess-themes/pieces/gothic/150/wk.png"
-                        class=".chess-piece" id="king" alt="white kring"/></td>
-                    <td class="light"><img src="https://images.chesscomfiles.com/chess-themes/pieces/gothic/150/wp.png"
-                            class=".chess-piece" id="pawn" alt="white pawn"/></td>
-                    <td class="dark"></td>
-                    <td class="light"><img src="https://images.chesscomfiles.com/chess-themes/pieces/gothic/150/bk.png"
-                            class=".chess-piece" id="king" alt="black kang"/></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"><img src="https://images.chesscomfiles.com/chess-themes/pieces/gothic/150/wk.png"
+                        className=".chess-piece" id="king" alt="white kring"/></td>
+                    <td className="light"><img src="https://images.chesscomfiles.com/chess-themes/pieces/gothic/150/wp.png"
+                            className=".chess-piece" id="pawn" alt="white pawn"/></td>
+                    <td className="dark"></td>
+                    <td className="light"><img src="https://images.chesscomfiles.com/chess-themes/pieces/gothic/150/bk.png"
+                            className=".chess-piece" id="king" alt="black kang"/></td>
                 </tr>
                 <tr>
-                    <td class="light">
-                        <div class="numeric-font numeric-pos dark-font">2</div>
+                    <td className="light">
+                        <div className="numeric-font numeric-pos dark-font">2</div>
                     </td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
-                    <td class="light"></td>
-                    <td class="dark"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
+                    <td className="light"></td>
+                    <td className="dark"></td>
                 </tr>
                 <tr>
-                    <td class="dark">
-                        <div class="numeric-font light-font">1</div>
-                        <div class="numeric-font letter-pos light-font">a</div>
+                    <td className="dark">
+                        <div className="numeric-font light-font">1</div>
+                        <div className="numeric-font letter-pos light-font">a</div>
                     </td>
-                    <td class="light">
-                        <div class="numeric-font letter-pos dark-font"> b</div>
+                    <td className="light">
+                        <div className="numeric-font letter-pos dark-font"> b</div>
                     </td>
-                    <td class="dark">
-                        <div class="numeric-font letter-pos light-font">c</div>
+                    <td className="dark">
+                        <div className="numeric-font letter-pos light-font">c</div>
 
                     </td>
-                    <td class="light">
-                        <div class="numeric-font letter-pos dark-font">d</div>
+                    <td className="light">
+                        <div className="numeric-font letter-pos dark-font">d</div>
                     </td>
-                    <td class="dark">
-                        <div class="numeric-font letter-pos light-font">e</div>
+                    <td className="dark">
+                        <div className="numeric-font letter-pos light-font">e</div>
                     </td>
-                    <td class="light">
-                        <div class="numeric-font letter-pos dark-font">f</div>
+                    <td className="light">
+                        <div className="numeric-font letter-pos dark-font">f</div>
                     </td>
-                    <td class="dark">
-                        <div class="numeric-font letter-pos light-font">g</div>
+                    <td className="dark">
+                        <div className="numeric-font letter-pos light-font">g</div>
                     </td>
-                    <td class="light">
-                        <div class="numeric-font letter-pos dark-font">h</div>
+                    <td className="light">
+                        <div className="numeric-font letter-pos dark-font">h</div>
                     </td>
                 </tr>
 
