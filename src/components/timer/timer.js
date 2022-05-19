8ab88ac8d3d1de9402c9bfdffe226afaf02d4480
Danylo <bodnar.dp@gmail.com>
2022-05-19 19:15:09 +0300

    timer_css_improvments

--- src/components/timer/timer.js
+++ src/components/timer/timer.js
@@ -10,27 +10,43 @@ class Timer extends Component{
             sec: 0,
         }
     }
+
+    onZero=()=>{
+        this.setState({
+            min: 0,
+            sec: 0
+        })
+    }
+
     render(){
         return(
             <div className="outer-timer-block" >
                 <div className="timer-block border-red radius-border">
-                    <span id="minutes" className="white-font">{this.state.min}</span>
+                    <span id="minutes" className="white-font">{this.state.min<10?'0'+this.state.min:this.state.min}</span>
                     <span className="white-font">:</span>
                     <span id="seconds" className="white-font">{this.state.sec<10?'0'+this.state.sec:this.state.sec}</span>
                 </div>
-                <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-                    <input type="radio" class="btn-check" name="timer-radio" id="10min-radio" autocomplete="off" checked/>
-                    <label class="btn btn-outline-primary" for="10min-radio">10хв</label>
-
-                    <input type="radio" class="btn-check" name="timer-radio" id="30min-radio" autocomplete="off"/>
-                    <label class="btn btn-outline-primary" for="30min-radio">30хв</label>
-
-                    <input type="radio" class="btn-check" name="timer-radio" id="60min-radio" autocomplete="off"/>
-                    <label class="btn btn-outline-primary" for="60min-radio">60хв</label>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="timer-radio" id="10radio"/>
+                    <label class="form-check-label" for="10radio">
+                        10хв
+                    </label>
                 </div>
-                <div class="btn-group" role="group" aria-label="Basic mixed styles example">
-                    <button type="button" class="btn btn-success">Старт</button>
-                    <button type="button" class="btn btn-danger">На нуль</button>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="timer-radio" id="30radio" checked/>
+                    <label class="form-check-label" for="30radio">
+                        30хв
+                    </label>
+                </div>
+                <div class="form-check">
+                    <input class="form-check-input" type="radio" name="timer-radio" id="60radio" checked/>
+                    <label class="form-check-label" for="60radio">
+                        60хв
+                    </label>
+                </div>
+                <div class="btn-group-start" role="group">
+                    <button type="button" class="btn-my btn-success">Старт</button>
+                    <button type="button" class="btn-my btn-danger" onClick={this.onZero}>На нуль</button>
                 </div>
             </div>
         );
